9b47fc2002fec6de263b2987e0c7f5a008e37a20
adidmccc <adieldan.1912@gmail.com>
2026-03-08 21:47:27 -0600

    main.c ejercicio3

--- movie/main.c
+++ movie/main.c
@@ -1,42 +1,10 @@
-#include <stdio.h>
 #include "pelicula.h"
 
-int main()
-{
-
-    printf("Prueba del TAD Pelicula\n");
-
-    /* TODO
-       Crear una película
-    */
-    Pelicula* p = NULL;
-
-
-    /* TODO
-       Agregar algunos directores
-    */
-
-
-    /* TODO
-       Imprimir película
-    */
-
-
-    printf("\nCambiar genero\n");
-
-    /* TODO
-       Cambiar genero
-    */
-
-
-    /* TODO
-       Imprimir nuevamente
-    */
-
-
-    /* TODO
-       Liberar memoria
-    */
-
+int main() {
+    pelicula mipelícula = crearpelicula("interstellar", "sci-fi", 2014);
+    agregardirector(mipelícula, "christopher nolan");
+    cambiargenero(mipelícula, "ciencia ficcion");
+    imprimirpelicula(mipelícula);
+    destruirpelicula(mipelícula);
     return 0;
 }
