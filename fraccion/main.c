6b21e34526ee822327392cc70a8ed1eb707869de
adidmccc <adieldan.1912@gmail.com>
2026-03-08 21:38:03 -0600

    main.c ejercicio2
    Refactor main function to create and sum fractions, and print results.

--- fraccion/main.c
+++ fraccion/main.c
@@ -1,43 +1,27 @@
-#include <stdio.h>
 #include "fraccion.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-
-    printf("Prueba del TAD Fraccion\n");
-
-    /* TODO
-       1 Crear dos fracciones
-       ejemplo: 1/2 y 3/4
-    */
-    Fraccion* f1 = NULL;
-    Fraccion* f2 = NULL;
-
+int main() {
+    Fraccion *f1, *f2, *res;
+    f1 = crearFraccion(2, 4);   
+    f2 = crearFraccion(1, 3);   
 
     printf("Fraccion 1: ");
-    /* TODO imprimir */
-
-
+    imprimir(f1);
     printf("Fraccion 2: ");
-    /* TODO imprimir */
+    imprimir(f2);
 
+    res = sumar(f1, f2);
+    printf("Resultado de la suma: ");
+    imprimir(res);
 
-    printf("\nSuma de fracciones:\n");
-
-    /* TODO
-       2 Sumar fracciones
-    */
-    Fraccion* resultado = NULL;
-
-
-    /* TODO
-       3 Imprimir resultado
-    */
-
-
-    /* TODO
-       4 Liberar memoria
-    */
+    destruir(f1);
+    destruir(f2);
+    destruir(res);
+    
+    f1 = NULL;
+    (f1 == NULL) ? printf("Memoria liberada correctamente.\n") : printf("Error.\n");
 
     return 0;
 }
